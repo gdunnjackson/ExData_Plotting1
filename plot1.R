1d5f6e7ec691fcf2eda8d858de2e8eab5a6730bd
Gloria Jackson <gdunnjackson@users.noreply.github.com>
2015-12-10 02:18:56 -0500

    Add Plot1.R and plot1.png to repository

---
+++ plot1.R
@@ -0,0 +1,31 @@
+###  BEGIN SCRIPT
+### NAME OF SCRIPT:  plot1.R
+### DESCRIPTION:  This script is used to perform analysis on the data collected
+##  from the household_power_consumption text file containing individual
+##  household consumption data.
+### AUTHOR: Gloria Jackson
+### DATE:   December 10, 2015
+
+## set working directory
+## NOTE: set working directory prior to executing script
+
+##  load libraries
+library(data.table)
+library(dplyr)
+library(datasets)
+
+##  read input data file ####
+epcdata <- data.table(read.delim("household_power_consumption.txt", header = TRUE, na.strings = "?",sep = ";"))
+epcsubset <- filter(epcdata[epcdata$Date == "1/2/2007" | epcdata$Date == "2/2/2007",])
+
+###  Plot the histogram on Screen
+hist(epcsubset$Global_active_power,main = "Global Active Power",col = "red",xlab = "Global Active Power (kilowatts)")      
+
+## Copy the histogram to a PNG file
+dev.copy(png,file = "plot1.png")   
+
+## Close the PNG device
+dev.off()                                        
+
+
+
